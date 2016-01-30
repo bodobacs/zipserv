91a5cd3df661d294949a0b620d2f5db086a25ab5
bodobacs <a@a.a>
2016-01-30 19:39:01 +0100

    Windows icon added.

--- src/resource.h
+++ src/resource.h
@@ -3,10 +3,11 @@
 #endif
 
 #define IDD_DIALOG1                             100
+#define IDR_MAINFRAME                           102
 #define ID_PORT                                 40000
 #define ID_SELECT                               40001
 #define ID_BROWSER                              40002
 #define ID_ZIPNAME                              40003
 #define ID_STOP                                 40004
-#define ID_START				40005
+#define ID_START                                40005
 #define ID_STATUS                               40006
